7e33c37163e2299f8064efbcdb44c017073135de
Saegl <saegl@protonmail.com>
2024-03-03 23:23:20 +0600

    Use transparent theme

--- lua/saegl/theme.lua
+++ lua/saegl/theme.lua
@@ -1,5 +1,16 @@
 require("rose-pine").setup({
-    variant = "main"
+    variant = "main",
+    highlight_groups = {
+		TelescopeBorder = { fg = "highlight_high", bg = "none" },
+		TelescopeNormal = { bg = "none" },
+		TelescopePromptNormal = { bg = "base" },
+		TelescopeResultsNormal = { fg = "subtle", bg = "none" },
+		TelescopeSelection = { fg = "text", bg = "base" },
+		TelescopeSelectionCaret = { fg = "rose", bg = "rose" },
+	},
+    styles = {
+        transparency = true,
+    },
 })
 
 vim.cmd('colorscheme rose-pine')
