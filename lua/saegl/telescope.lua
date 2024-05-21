473bf2976fb93ae94347d897acc01f6dd27e6803
Saegl <saegl@protonmail.com>
2024-05-21 23:44:36 +0500

    Shorten telescope keymaps

--- lua/saegl/telescope.lua
+++ lua/saegl/telescope.lua
@@ -1,9 +1,9 @@
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-vim.keymap.set('n', '<leader>sd', builtin.diagnostics, {})
+vim.keymap.set('n', '<leader>f', builtin.find_files, { desc = "Find files" })
+vim.keymap.set('n', '<leader>g', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>b', builtin.buffers, {})
+vim.keymap.set('n', '<leader>d', builtin.diagnostics, {})
+
 vim.keymap.set("n", "<leader>m", function() vim.cmd("Telescope projects") end)
 
 require("project_nvim").setup({})
