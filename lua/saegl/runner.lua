2414331c26f8fd307fb524b9e204b6fd5760ccd8
Saegl <saegl@protonmail.com>
2024-05-21 23:46:54 +0500

    Add "just" runner

---
+++ lua/saegl/runner.lua
@@ -0,0 +1,2 @@
+vim.keymap.set('n', '<leader>r', function() vim.cmd("!just run") end, { desc = "just run" })
+vim.keymap.set('n', '<leader>t', function() vim.cmd("!just test") end, { desc = "just test" })
