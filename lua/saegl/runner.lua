564192561ec69db2d64359208be72813c49ec0c5
Saegl <saegl@protonmail.com>
2024-05-29 10:06:35 +0500

    Remove just runner

--- lua/saegl/runner.lua
+++
@@ -1,2 +0,0 @@
-vim.keymap.set('n', '<leader>r', function() vim.cmd("!just run") end, { desc = "just run" })
-vim.keymap.set('n', '<leader>t', function() vim.cmd("!just test") end, { desc = "just test" })
