92726408568ecfc02fa5ade9fb2bffd9cf25899e
Saegl <saegl@protonmail.com>
2024-05-29 10:33:12 +0500

    Refactor old plugins

--- init.lua
+++ init.lua
@@ -13,12 +13,3 @@ require 'neovide'
 
 require 'lazy-bootstrap'
 require 'lazy-plugins'
-
--- require("saegl.plugins")
--- require("saegl.theme")
--- require("saegl.harpoon")
--- require("saegl.lsp")
--- require("saegl.dap")
--- require("saegl.autocomplete")
--- require("saegl.runner")
--- require("saegl.whichkey")
--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -1,3 +1,56 @@
 require 'lazy'.setup {
-    { import = 'custom.plugins' }
+    -- Load plugins from `lua/custom/plugins/*.lua`
+    { import = 'custom.plugins' },
+
+    'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
+    { 'numToStr/Comment.nvim',  opts = {} }, -- Add comments with 'gcc'
+    {
+        "nvim-neo-tree/neo-tree.nvim",
+        branch = "v3.x",
+        config = function()
+            vim.keymap.set('n', '<C-b>', '<Cmd>Neotree toggle<CR>')
+        end,
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+            "MunifTanjim/nui.nvim",
+            -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+        }
+    },
+    {
+        'akinsho/toggleterm.nvim',
+        version = "*",
+        opts = {
+            size = 20,
+            open_mapping = [[<c-`>]],
+        }
+    },
+    {
+        'ggandor/leap.nvim',
+        config = function()
+            require('leap').create_default_mappings()
+        end,
+    },
+    {
+        'nvim-lualine/lualine.nvim',
+        dependencies = { 'nvim-tree/nvim-web-devicons' },
+        config = function()
+            require('lualine').setup()
+        end,
+    },
+    -- Git integration
+    { 'tpope/vim-fugitive' },
+    {
+        'Julian/lean.nvim',
+        event = { 'BufReadPre *.lean', 'BufNewFile *.lean' },
+
+        dependencies = {
+            'neovim/nvim-lspconfig',
+            'nvim-lua/plenary.nvim',
+        },
+
+        opts = {
+            mappings = true,
+        }
+    }
 }
