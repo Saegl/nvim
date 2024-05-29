1fa2f32797456d6079677e43b583fec2788a0bfd
Saegl <saegl@protonmail.com>
2024-05-29 11:50:01 +0500

    Disable neovide fullscreen

--- lua/neovide.lua
+++ lua/neovide.lua
@@ -4,9 +4,8 @@ vim.g.neovide_hide_mouse_when_typing = true
 vim.g.neovide_refresh_rate = 165
 vim.g.neovide_refresh_rate_idle = 5
 vim.g.neovide_profiler = false
-vim.g.neovide_cursor_vfx_mode = ""  -- set 'pixiedust' to enable
+vim.g.neovide_cursor_vfx_mode = "" -- set 'pixiedust' to enable
 vim.g.neovide_cursor_vfx_opacity = 500.0
 vim.g.neovide_cursor_vfx_particle_lifetime = 3.0
 vim.g.neovide_cursor_vfx_particle_density = 100.0
-vim.g.neovide_fullscreen = true
-
+vim.g.neovide_fullscreen = false
