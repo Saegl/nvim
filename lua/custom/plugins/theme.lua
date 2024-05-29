ad4e025f8e2d7302c1eccf4fd6d1ad7a328c1944
Saegl <saegl@protonmail.com>
2024-05-29 10:19:48 +0500

    Refactor theme

---
+++ lua/custom/plugins/theme.lua
@@ -0,0 +1,24 @@
+return {
+    {
+        'rose-pine/neovim',
+        name = 'rose-pine',
+        config = function()
+            require("rose-pine").setup({
+                variant = "main",
+                highlight_groups = {
+                    TelescopeBorder = { fg = "highlight_high", bg = "none" },
+                    TelescopeNormal = { bg = "none" },
+                    TelescopePromptNormal = { bg = "base" },
+                    TelescopeResultsNormal = { fg = "subtle", bg = "none" },
+                    TelescopeSelection = { fg = "text", bg = "base" },
+                    TelescopeSelectionCaret = { fg = "rose", bg = "rose" },
+                },
+                styles = {
+                    transparency = true,
+                },
+            })
+
+            -- vim.cmd('colorscheme rose-pine')
+        end,
+    },
+}
