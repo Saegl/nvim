21f7f526afa099a054457435b271315f93de2020
Saegl <saegl@protonmail.com>
2024-05-29 16:46:03 +0500

    Add more colorschemes

--- lua/custom/plugins/theme.lua
+++ lua/custom/plugins/theme.lua
@@ -76,7 +76,7 @@ return {
         name = "catppuccin",
         priority = 1000,
         config = function()
-            vim.cmd.colorscheme "catppuccin-mocha"
+            -- vim.cmd.colorscheme "catppuccin-mocha"
         end,
     },
     {
@@ -86,4 +86,60 @@ return {
             -- vim.cmd.colorscheme "oh-lucy-evening"
         end,
     },
+    {
+        "HoNamDuong/hybrid.nvim",
+        lazy = false,
+        priority = 1000,
+        config = function()
+            -- vim.cmd.colorscheme "hybrid"
+        end,
+    },
+    {
+        'projekt0n/github-nvim-theme',
+        lazy = false,    -- make sure we load this during startup if it is your main colorscheme
+        priority = 1000, -- make sure to load this before all the other start plugins
+        config = function()
+            require('github-theme').setup({
+                -- ...
+            })
+
+            -- vim.cmd('colorscheme github_dark_tritanopia')
+            -- vim.cmd('colorscheme github_dark_high_contrast')
+        end,
+    },
+    {
+        'marko-cerovac/material.nvim',
+        lazy = true,
+        priority = 1000,
+        config = function()
+            vim.g.material_style = "deep ocean"
+            -- vim.cmd.colorscheme 'material'
+        end,
+    },
+    {
+        'olivercederborg/poimandres.nvim',
+        lazy = false,
+        priority = 1000,
+        config = function()
+            require('poimandres').setup {
+                -- leave this setup function empty for default config
+                -- or refer to the configuration section
+                -- for configuration options
+            }
+            -- vim.cmd.colorscheme "poimandres"
+        end,
+    },
+    {
+        "EdenEast/nightfox.nvim",
+        lazy = false,
+        priority = 1000,
+        config = function()
+            require('nightfox').setup {
+                options = {
+                    dim_inactive = true,
+                }
+            }
+            vim.cmd.colorscheme 'carbonfox'
+        end,
+    }
 }
