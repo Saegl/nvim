cb9a3261143ece45212f1ed7e569521032342de8
Saegl <saegl@protonmail.com>
2024-06-03 22:25:52 +0500

    Remove lsp format keymap

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -21,8 +21,6 @@ return {
                         vim.keymap.set('n', keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
                     end
 
-                    map('<F3>', vim.lsp.buf.format, 'Format file')
-
                     -- Jump to the definition of the word under your cursor.
                     --  This is where a variable was first declared, or where a function is defined, etc.
                     --  To jump back, press <C-t>.
