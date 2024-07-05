b878fc7695ff6150ad74e450af4985bce74cc570
Saegl <saegl@protonmail.com>
2024-07-05 17:18:12 +0500

    Add more bufferline keymaps

--- lua/saegl/plugins/bufferline.lua
+++ lua/saegl/plugins/bufferline.lua
@@ -32,16 +32,69 @@ return {
         config = function(_, opts)
             local bufferline = require 'bufferline'
             bufferline.setup(opts)
-            vim.keymap.set('n', '<leader>bd', '<cmd>bd<cr>')
-            vim.keymap.set('n', '<leader>bp', '<cmd>BufferLinePick<cr>')
-            vim.keymap.set('n', '<leader>bc', '<cmd>BufferLinePickClose<cr>')
-            vim.keymap.set('n', '<leader>bo', '<cmd>BufferLineCloseOthers<cr>')
+            vim.keymap.set(
+                'n',
+                '<leader>bd',
+                '<cmd>bd<cr>',
+                { desc = "Close buffer" }
+            )
+            vim.keymap.set(
+                'n',
+                '<leader>bp',
+                '<cmd>BufferLinePick<cr>',
+                { desc = "Pick buffer" }
+            )
+            vim.keymap.set(
+                'n',
+                '<leader>bc',
+                '<cmd>BufferLinePickClose<cr>',
+                { desc = "Pick buffer to close" }
+            )
+            vim.keymap.set(
+                'n',
+                '<leader>bo',
+                '<cmd>BufferLineCloseOthers<cr>',
+                { desc = "Close other buffers" }
+            )
+            vim.keymap.set(
+                'n',
+                '<leader>bn',
+                '<cmd>BufferLineTogglePin<cr>',
+                { desc = "Pin current buffer" }
+            )
+            vim.keymap.set(
+                'n',
+                '<leader>bu',
+                '<cmd>BufferLineGroupClose ungrouped<cr>',
+                { desc = "Close ungrouped" }
+            )
 
-            vim.keymap.set('n', '<Tab>', '<cmd>BufferLineCycleNext<cr>')
-            vim.keymap.set('n', '<S-Tab>', '<cmd>BufferLineCyclePrev<cr>')
+            vim.keymap.set(
+                'n',
+                '<Tab>',
+                '<cmd>BufferLineCycleNext<cr>',
+                { desc = "Next buffer" }
+            )
+            vim.keymap.set(
+                'n',
+                '<S-Tab>',
+                '<cmd>BufferLineCyclePrev<cr>',
+                { desc = "Previous buffer" }
+            )
 
-            for i = 1, 4, 1 do
-                vim.keymap.set('n', '<F' .. i .. '>', '<cmd>BufferLineGoToBuffer' .. i .. '<cr>')
+            for i = 1, 9, 1 do
+                vim.keymap.set(
+                    'n',
+                    '<M-' .. i .. '>',
+                    function() bufferline.go_to(i, true) end,
+                    { desc = "Open buffer " .. i }
+                )
+                vim.keymap.set(
+                    'n',
+                    '<leader>bm' .. i,
+                    function() bufferline.move_to(i) end,
+                    { desc = "Move buffer to " .. i }
+                )
             end
         end,
     },
