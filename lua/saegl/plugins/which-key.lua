fb3c549ec465f926486640ff035cd2dac43e0bf3
Saegl <saegl@protonmail.com>
2024-08-03 16:42:01 +0500

    Fix which-key deprecations, almost

--- lua/saegl/plugins/which-key.lua
+++ lua/saegl/plugins/which-key.lua
@@ -3,10 +3,13 @@ return {
         'folke/which-key.nvim',
         event = 'VimEnter',
         config = function()
-            require('which-key').setup()
+            local wk = require('which-key')
+            wk.setup {
+                notify = false,
+            }
 
             -- Document existing key chains
-            require('which-key').register {
+            wk.register {
                 ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
                 ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
                 ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
@@ -16,7 +19,7 @@ return {
                 ['<leader>h'] = { name = 'Git [H]unk', _ = 'which_key_ignore' },
             }
             -- visual mode
-            require('which-key').register({
+            wk.register({
                 ['<leader>h'] = { 'Git [H]unk' },
             }, { mode = 'v' })
         end,
